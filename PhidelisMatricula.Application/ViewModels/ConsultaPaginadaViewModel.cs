78afab628075d2accd0cff6f6221a173f1d1070b
Lubert Henrique <lubert-h@hotmail.com>
2022-09-05 17:53:00 -0300

    Ajustes ordem de listagem dados de consultas paginadas

--- PhidelisMatricula.Application/ViewModels/ConsultaPaginadaViewModel.cs
+++ PhidelisMatricula.Application/ViewModels/ConsultaPaginadaViewModel.cs
@@ -3,10 +3,10 @@
 namespace PhidelisMatricula.Core.Application.ViewModels
 {
     public class ConsultaPaginadaViewModel<T>
-    {
-        public IList<T> Dados { get; set; }
+    {        
         public int Pagina { get; set; }
         public int QuantidadePorPagina { get; set; }
         public int Total { get; set; }
+        public IList<T> Dados { get; set; }
     }
 }
